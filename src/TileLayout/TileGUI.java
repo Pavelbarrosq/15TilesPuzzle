3315ffa2b34a2116e59eb4eb7e1955d20c967296
Pavel Barros Quintanilla <Pavelbarrosq@gmail.com>
2020-10-27 14:07:27 +0100

    Changed gui and added random buttons to game pane.

--- src/TileLayout/TileGUI.java
+++ src/TileLayout/TileGUI.java
@@ -1,44 +1,79 @@
 package TileLayout;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class TileGUI extends JFrame {
-    private int rows = 4;
-    private int cols = 4;
-    private JButton buttons[];
-    private JPanel gamePanel = new JPanel();
+    private static final int size = 16;
+    private JButton buttons[][] = new JButton[4][4];
+    private JPanel gamePanel;
     private JPanel buttonPanel = new JPanel();
     private JButton newGame = new JButton("New game");
-    private JButton endGame = new JButton("End game");
+
+    public ArrayList<Integer> numberList = new ArrayList<>(size);
+    public int emptyCell;
 
     public TileGUI() {
-        buttons = new JButton[16];
+        buttonPanel.add(newGame);
+        newGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Tar bort game pane och skapar upp ett nytt igen.
+                remove(gamePanel);
+                startNewGame();
+                repaint();
+            }
+        });
 
         add(buttonPanel, BorderLayout.NORTH);
-        buttonPanel.add(newGame);
-        newGame.addActionListener(e -> {
-        });
-        buttonPanel.add(endGame);
-        endGame.addActionListener(e -> {
-        });
-
-        setSize(500, 500);
-        setMinimumSize(new Dimension(500, 500));
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 400);
         setVisible(true);
 
-        add("South", gamePanel);
-        gamePanel.setLayout(new GridLayout(rows, cols));
-        gamePanel.setPreferredSize(new Dimension(400, 400));
-        for (int i = 1; i < 16; i++) {
-            buttons[i] = new JButton(String.valueOf(i));
-            gamePanel.add(buttons[i]);
+        startNewGame();
+    }
+
+    private void startNewGame() {
+        numberList.clear();
+        gamePanel = new JPanel();
+        System.out.println(numberList);
+
+        //Addar nummer från 1-15 till listan och sedan shufflar det.
+        for (int i = 0; i < size; i++) {
+            numberList.add(i, i);
+        }
+        Collections.shuffle(numberList);
+
+        for (int i = 0; i < size; i++) {
+            int col = i % 4;
+            int row = i / 4;
+            buttons[row][col] = new JButton(String.valueOf(numberList.get(i)));
+            System.out.println(numberList.get(i));
+
+            if (numberList.get(i) == 0) {
+                emptyCell = i;
+                buttons[row][col].setVisible(false);
+            }
+
+            //Stylar text och lägger till actionlistener till knapparna.
+            buttons[row][col].setForeground(Color.black);
+            buttons[row][col].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("TAPPED: " + e.getSource());
+                }
+            });
+
+            gamePanel.setLayout(new GridLayout(4, 4));
+            gamePanel.add(buttons[row][col]);
         }
 
+        add(gamePanel, BorderLayout.CENTER);
+        repaint();
     }
 
 }
